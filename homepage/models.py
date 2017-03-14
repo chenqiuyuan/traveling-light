6815d0f96832177644bd093a3800f1a70944bca7
chenqiuyuan <380133194@qq.com>
2017-03-14 20:27:28 +0800

    add an admin

--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=30)
+    capital = models.CharField(max_length=30)
+
--- traveler/urls.py
+++ traveler/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-
 urlpatterns = [
+    url(r'^', include('homepage.urls')),
     url(r'^homepage/', include('homepage.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
